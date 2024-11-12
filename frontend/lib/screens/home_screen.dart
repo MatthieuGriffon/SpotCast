f9d70646a4c333ce3b87a40ca450076743845a64
MatthieuGriffon <postmaster@matthieu-griffon.fr>
2024-11-12 23:04:02 +0100

    feat: Add image carousel with titles and styling
    - Integrated an image carousel on the home page using ListView.builder
    - Added overlay titles to carousel images to highlight key sections (Discover spots, Join a group, Plan your trip)
    - Fixed alignment issues to ensure all carousel images have the same size
    - Styled the text Explore our app with a semi-transparent black background, rounded corners, and a shadow effect
    - Optimized the code by removing unnecessary const keywords to fix issues with dynamic widgets

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
+import '../widgets/common/carousel_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Obtenir la largeur de l'écran
     final double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      // On supprime l'AppBar pour que l'image de fond prenne tout l'écran
       body: Stack(
         children: [
           // Image de fond
           Positioned.fill(
             child: Image.asset(
-              'assets/images/realistic_sunrise.webp', // Remplace par le chemin correct vers ton image
+              'assets/images/realistic_sunrise.webp',
               fit: BoxFit.cover,
             ),
           ),
-          // Contenu principal
           Positioned(
-            top: 60, // Ajuster pour remonter le texte si nécessaire
+            top: 60,
             left: 20,
             right: 20,
             child: Column(
@@ -38,19 +36,108 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20),
-                // Texte de description limité à un tiers de la largeur de l'écran
+
+                // Texte de description
                 SizedBox(
                   width: screenWidth / 1.5,
                   child: const Text(
                     'Découvrez les meilleurs spots de pêche autour de vous.',
                     style: TextStyle(
-                      fontFamily: 'Poppins',
+                      fontFamily: 'Roboto',
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
                       color: Color(0xCC000000),
                     ),
                   ),
                 ),
+                const SizedBox(height: 40),
+
+                // Boutons "Découvrir" et "Rejoindre"
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(207, 255, 255, 255),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 26, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                      ),
+                      child: const Text(
+                        'Inscription',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1B3A57),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(207, 255, 255, 255),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                      ),
+                      child: const Text(
+                        'Connexion',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1B3A57),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 110),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Container(
+                      // Retirer 'const' ici
+                      padding: const EdgeInsets.all(
+                          10), // Espace interne autour du texte
+                      decoration: BoxDecoration(
+                        color: Colors.black
+                            .withOpacity(0.7), // Fond noir avec opacité
+                        borderRadius:
+                            BorderRadius.circular(12), // Coins arrondis
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(0xFF9AACA9),
+                            blurRadius: 6,
+                            offset: Offset(0, 3), // Décalage ombre
+                          ),
+                        ],
+                      ),
+                      child: const Text(
+                        'Parcourez notre application',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Monsterrat',
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white, // Couleur du texte en blanc
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Ajout du carrousel d'images
+                const ImageCarousel(),
               ],
             ),
           ),
---
+++ frontend/lib/widgets/common/carousel_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class ImageCarousel extends StatelessWidget {
+  const ImageCarousel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> images = [
+      'assets/images/image_slider01.webp',
+      'assets/images/image_slider02.webp',
+      'assets/images/image_slider03.webp',
+    ];
+
+    final List<String> titles = [
+      'Découvrez des spots',
+      'Rejoignez un groupe',
+      'Planifiez votre sortie'
+    ];
+
+    return SizedBox(
+      height: 110, // Hauteur ajustée pour le carrousel
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: 90, // Largeur ajustée pour voir les images côte à côte
+            margin: EdgeInsets.only(
+              left: index == 0 ? 0 : 8,
+              right: 14,
+              top: 20,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Stack(
+                children: [
+                  // Image
+                  AspectRatio(
+                    aspectRatio: 1, // Maintient le ratio carré
+                    child: Image.asset(
+                      images[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  // Superposition du texte
+                  Positioned(
+                    bottom: 20,
+                    left: 7,
+                    right: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          titles[index],
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 8,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
